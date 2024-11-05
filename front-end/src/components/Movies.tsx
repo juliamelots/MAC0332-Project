818c4d6a10b26bc7cebd20883045a3760e28d6c0
juliamelots <juliamelots@ibm.com>
2024-11-05 17:01:34 -0300

    Set page to take up whole screen

---
+++ front-end/src/components/Movies.tsx
@@ -0,0 +1,15 @@
+function Movies() {
+  return (
+    <div className="h-100 d-flex flex-column">
+      <div className="w-100 h-25">
+
+      </div>
+
+      <div className="w-100 h-75">
+
+      </div>
+    </div>
+  )
+}
+
+export default Movies
