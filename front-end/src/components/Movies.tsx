46b1026f88d1d1ec448718f50576cbd2093c3b28
juliamelots <juliamelots@ibm.com>
2024-11-06 09:12:19 -0300

    Draft page with movie cards

--- front-end/src/components/Movies.tsx
+++ front-end/src/components/Movies.tsx
@@ -1,12 +1,24 @@
 function Movies() {
-  return (
-    <div className="h-100 d-flex flex-column">
-      <div className="w-100 h-25">
+  const movies = [
+    { title: 'The Bridges of Madison County', id: 1 },
+    { title: 'Forrest Gump', id: 2 },
+    { title: 'Top Gun', id: 3 },
+  ];
 
+  const moviesGrid = movies.map(movie =>
+    <div style={{ width: '100px', height: '80px' }}>
+      {/* {movie.title} */}
+    </div>
+  );
+
+  return (
+    <div className="h-100 d-flex flex-column bg-black">
+      <div className="w-100 h-25 bg-red">
+        <h1 className="text-center text-white">Filmes</h1>
       </div>
 
-      <div className="w-100 h-75">
-
+      <div className="w-100 h-75 d-flex flex-row flex-wrap">
+        {moviesGrid}
       </div>
     </div>
   )
