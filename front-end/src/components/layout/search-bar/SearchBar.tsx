f1530ccdd2fe744cf096757d31a96787f35eff66
juliamelots <juliamelots@ibm.com>
2024-11-12 00:02:51 -0300

    Add search mechanism to search bar component

--- front-end/src/components/layout/search-bar/SearchBar.tsx
+++ front-end/src/components/layout/search-bar/SearchBar.tsx
@@ -1,20 +1,38 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 
 import './SearchBar.css';
 
 interface SearchBarProps {
   placeholder: string;
   showFilter: boolean;
+  searchInput: string;
+  setSearchInput: (newValue: string) => void;
 }
 
 const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
+  const [tempSearchInput, setTempSearchInput] = useState(props.searchInput);
+
+  const onSearchInputChanged = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setTempSearchInput(event.target.value);
+  };
+
+  const onSearchButtonClicked = (): void => {
+    props.setSearchInput(tempSearchInput);
+  };
+
   return (
     <div className="input-group my-1 py-1 d-flex justify-content-center align-center w-100">
       <div id="search-input" className="w-75 bg-ac-red">
         <div className="input-group col-md-12">
-          <input type="text" className="form-control input-lg" placeholder={props.placeholder} />
+          <input className="form-control input-lg"
+            type="text"
+            placeholder={props.placeholder}
+            value={tempSearchInput}
+            onChange={onSearchInputChanged}
+          />
           <span className="input-group-btn">
-              <button className="btn btn-ac-red text-white" type="button">
+              <button className="btn btn-ac-red text-white" type="button"
+                onClick={onSearchButtonClicked}>
                 <i className="bi bi-search btn-icon"></i>
               </button>
               {props.showFilter ? (
