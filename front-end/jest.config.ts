c372b23eb3724179fe5818b85ab4ce3e86dd41c1
juliamelots <juliamelots@ibm.com>
2024-11-12 16:33:07 -0300

    Add jest configurations and dependencies

--- front-end/jest.config.ts
+++ front-end/jest.config.ts
@@ -4,6 +4,13 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '\\.css$': 'jest-transform-css',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
 };
 
