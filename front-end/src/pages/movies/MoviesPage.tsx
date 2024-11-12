953db5daa8a7953c0794effafb3e6307d7ea86d8
juliamelots <juliamelots@ibm.com>
2024-11-12 00:03:24 -0300

    Apply search bar mechanism to movies search by title

--- front-end/src/pages/movies/MoviesPage.tsx
+++ front-end/src/pages/movies/MoviesPage.tsx
@@ -1,3 +1,5 @@
+import { React, useState } from 'react';
+
 import Navbar from '@/components/layout/navbar/Navbar';
 import SearchBar from '@/components/layout/search-bar/SearchBar';
 
@@ -11,12 +13,20 @@ function MoviesPage() {
     { title: 'Top Gun: Maverick', posterUrl: 'https://br.web.img3.acsta.net/c_310_420/pictures/19/12/16/15/00/5548914.jpg' },
   ];
 
-  const moviesGrid = movies.map((movie, idx) =>
-    <div key={idx} className="movie-poster-size position-relative d-flex justify-content-center">
-      <img src={movie.posterUrl} alt={'Cartaz de "' + movie.title + '"'} className="movie-poster-size movie-poster" />
-      <p className="movie-title text-center">{movie.title}</p>
-    </div>
-  );
+  const [searchInput, setSearchInput] = useState('');
+
+  const moviesGrid = movies
+    .filter((movie) => {
+      const lowerMovieTitle = movie.title.toLocaleLowerCase();
+      const lowerSearchInput = searchInput.toLocaleLowerCase();
+      return searchInput === '' || lowerMovieTitle.includes(lowerSearchInput);
+    })
+    .map((movie, idx) =>
+      <div key={idx} className="movie-poster-size position-relative d-flex justify-content-center">
+        <img src={movie.posterUrl} alt={'Cartaz de "' + movie.title + '"'} className="movie-poster-size movie-poster" />
+        <p className="movie-title text-center">{movie.title}</p>
+      </div>
+    );
 
   return (
     <>
@@ -24,7 +34,12 @@ function MoviesPage() {
       <div className="h-100 d-flex flex-column">
         <div className="w-100 h-25 bg-ac-red">
           <h1 className="text-center text-white mt-2 mb-5">Filmes</h1>
-          <SearchBar placeholder="Procure filmes por título" showFilter={true}/> {/* TODO: Add searching mechanism */}
+          <SearchBar
+            placeholder="Procure filmes por título"
+            showFilter={true}
+            searchInput={searchInput}
+            setSearchInput={setSearchInput}
+          />
         </div>
 
         <div id="movies-container" className="w-100 h-75 bg-ac-black">
