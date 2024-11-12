708a4c591b8a4001590032404fa9891f9e72eea2
Francisco Henriques <franciscolealhenriques@usp.br>
2024-11-11 22:27:52 -0300

    Update folder name from "com.example" to "com.rotadacultura"

---
+++ backend/src/main/kotlin/com/rotadacultura/Application.kt
@@ -0,0 +1,17 @@
+package com.rotadacultura
+
+import com.rotadacultura.plugins.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+    configureRouting()
+}
