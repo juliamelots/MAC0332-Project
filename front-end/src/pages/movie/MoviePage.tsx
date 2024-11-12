4421e17c4e2324dce09591940f60c6f2ba651cd2
juliamelots <juliamelots@ibm.com>
2024-11-12 16:34:02 -0300

    Fix app component test suggestions

--- front-end/src/pages/movie/MoviePage.tsx
+++ front-end/src/pages/movie/MoviePage.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function MoviePage() {
   return (
     <div>Filme específico</div>
--- front-end/src/pages/movies/MoviesPage.tsx
+++ front-end/src/pages/movies/MoviesPage.tsx
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import { useState } from 'react';
 
 import Navbar from '@/components/layout/navbar/Navbar';
 import SearchBar from '@/components/layout/search-bar/SearchBar';
