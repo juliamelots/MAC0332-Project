a7cee7d725c60b88629c546128e5f7eef3878f3d
Francisco Henriques <franciscolealhenriques@usp.br>
2024-11-11 22:31:45 -0300

    Add tests to movie route
    Tests to get the list of movies on display and the cinemas displaying
    them were added.

--- backend/src/test/kotlin/MovieRoutesTest.kt
+++ backend/src/test/kotlin/MovieRoutesTest.kt
@@ -1,2 +1,54 @@
+package com.rotadacultura
+
+import com.rotadacultura.model.Movie
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlinx.serialization.json.Json
+import org.junit.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
 class MovieRoutesTest {
+    @Test
+    fun listOfMoviesIsCorrectlyReturned() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/movie")
+        val body = response.bodyAsText()
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertContains(body, "Divertida Mente 2")
+    }
+
+    @Test
+    fun cinemaIsCreatedOnMovieGetRequest() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/movie")
+        println(response.bodyAsText())
+        assertEquals(HttpStatusCode.OK, response.status)
+
+        // Deserialize response body to access the availableCinemas field
+        val json = Json { ignoreUnknownKeys = true }
+        val movieResponse = json.decodeFromString<Map<String, List<Movie>>>(response.bodyAsText())
+        val movies = movieResponse["movies"] ?: emptyList()
+        val firstMovie = movies.firstOrNull()
+        assertNotNull(firstMovie)
+
+        val availableCinemas = firstMovie.availableCinemas
+        assertNotNull(availableCinemas)
+
+        val cinema = availableCinemas.firstOrNull { it.name == "Cinemark" }
+        assertNotNull(cinema)
+        assertEquals("Cinemark", cinema.name)
+        assertEquals(5.23587, cinema.latitude)
+        assertEquals(23.18275, cinema.longitude)
+    }
 }
