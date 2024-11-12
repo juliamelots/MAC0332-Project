ce2c9a7d31017fbd204139f0efaf05055c772d93
Yuki Sano <clayuki@amazon.com>
2024-11-12 12:55:00 -0300

    Add new colors to index.css

--- front-end/src/pages/cinemas/CinemaCard.tsx
+++ front-end/src/pages/cinemas/CinemaCard.tsx
@@ -24,7 +24,7 @@ const CinemaCard = ({
   commuteInfo,
 }: CinemaCardProps) => {
   return (
-    <div className="card my-4 p-4 card-container">
+    <div className="card my-4 p-4 card-container bg-ac-white">
       <div className="d-flex flex-row justify-content-between">
         <div>
           <h5 className="text-dark">{name}</h5>
@@ -32,7 +32,7 @@ const CinemaCard = ({
         </div>
         <div className="d-flex flex-column my-1">
           <div className="d-flex align-items-center">
-            <i class="bi bi-ticket-detailed generic-info-icon"></i>
+            <i className="bi bi-ticket-detailed generic-info-icon icon-bg-purple"></i>
             <span className="text-muted ms-3">Preço</span>
           </div>
           <div>
@@ -46,14 +46,14 @@ const CinemaCard = ({
         {/* Left column for location info */}
         <div className="flex-grow-1">
           <div className="d-flex align-items-center my-1">
-            <i class="bi bi-circle-fill home-address-icon"></i>
+            <i className="bi bi-circle-fill home-address-icon icon-bg-grey"></i>
             <span className="text-muted ms-3">{address.home.street}</span>
           </div>
           <div>
             <p className="text-muted ms-5">{address.home.city}</p>
           </div>
           <div className="d-flex align-items-center my-1">
-            <i class="bi bi-geo-alt-fill destination-address-icon"></i>
+            <i className="bi bi-geo-alt-fill destination-address-icon icon-bg-red"></i>
             <span className="text-muted ms-3">{address.destination.street}</span>
           </div>
           <div>
@@ -62,7 +62,7 @@ const CinemaCard = ({
         </div>
 
         {/* Right column for subs info */}
-        <div className="">
+        <div>
           <div className="text-end my-1">
             <p className="text-muted mb-1">Legendado</p>
             <div className="d-flex gap-2">
@@ -95,9 +95,9 @@ const CinemaCard = ({
       <div className="d-flex justify-content-start my-3" style={{ gap: '80px' }}>
         <div className="d-flex flex-column">
           <div className="d-flex align-items-center my-1">
-            <i class="bi bi-clock-fill generic-info-icon"></i>
+            <i className="bi bi-clock-fill generic-info-icon icon-bg-purple"></i>
             <span className="text-muted ms-3">Melhor trajeto</span>
-            <span className="ms-3"><i class="bi bi-bus-front transportation-icon"></i></span>
+            <span className="ms-3"><i className="bi bi-bus-front transportation-icon icon-bg-yellow"></i></span>
           </div>
           <div>
             <h6 className="text-dark ms-5">{commuteInfo.bestRoute.time}</h6>
@@ -105,7 +105,7 @@ const CinemaCard = ({
         </div>
         <div className="d-flex flex-column">
           <div className="d-flex align-items-center my-1">
-            <i class="bi bi-map generic-info-icon"></i>
+            <i className="bi bi-map generic-info-icon icon-bg-purple"></i>
             <span className="text-muted ms-3">Menor distância</span>
           </div>
           <div>
