ff23891503857ebf2aa48377bc383e9ff6bbdf49
natalyaaragao <natalya.saragao@gmail.com>
2024-11-13 10:51:14 -0300

    Fix className

--- front-end/src/pages/movie/MovieInfoContent.tsx
+++ front-end/src/pages/movie/MovieInfoContent.tsx
@@ -7,51 +7,51 @@ interface props {
 };
 
 const MovieInfoContent = ({ plot, cast, genre }: props) => {
-    const [activeTab, setActiveTab] = useState('tab1');
+    const [activeTab, setActiveTab] = useState('tabPlot');
     const actors = cast.split(",");
     return (
         <div>
             <ul className="nav nav-tabs">
-            <li className="nav-item">
-              <span 
-                role="button"
-                className={`cursor-pointer nav-link ${activeTab === 'tab1' ? 'active' : ''}`}
-                onClick={() => setActiveTab('tab1')}
-              >
-                Sinopse
-              </span>
-            </li>
-            <li className="nav-item">
-              <span 
-                role="button"
-                className={`nav-link ${activeTab === 'tab2' ? 'active' : ''}`}
-                onClick={() => setActiveTab('tab2')}
-              >
-                Elenco
-              </span>
-            </li>
-            <li className="nav-item">
-              <span 
-                role="button"
-                className={`nav-link ${activeTab === 'tab3' ? 'active' : ''}`}
-                onClick={() => setActiveTab('tab3')}
-              >
-                Gênero
-              </span>
-            </li>
+                <li className="nav-item" key={1}>
+                    <span 
+                      role="button"
+                      className={`cursor-pointer nav-link ${activeTab === 'tabPlot' ? 'active' : ''}`}
+                      onClick={() => setActiveTab('tabPlot')}
+                    >
+                      Sinopse
+                    </span>
+                </li>
+                <li className="nav-item" key={2}>
+                    <span 
+                      role="button"
+                      className={`nav-link ${activeTab === 'tabCast' ? 'active' : ''}`}
+                      onClick={() => setActiveTab('tabCast')}
+                    >
+                      Elenco
+                    </span>
+                </li>
+                <li className="nav-item" key={3}>
+                    <span 
+                      role="button"
+                      className={`nav-link ${activeTab === 'tabGenre' ? 'active' : ''}`}
+                      onClick={() => setActiveTab('tabGenre')}
+                    >
+                      Gênero
+                    </span>
+                </li>
           </ul>
 
-        <div className="tab-content mt-3">
-            {activeTab === 'tab1' && <div className="text-ac-white">{plot}</div>}
-            {activeTab === 'tab2' && 
-                <div className="text-ac-white d-flex flex-wrap">
-                    { actors.map((actor) => (
-                        <p className="movie-card-cast">{actor}</p>
-                    )) }
-                </div>
-            }
-            {activeTab === 'tab3' && <div className="text-ac-white">{genre}</div>}
-        </div>
+          <div className="tab-content mt-3">
+              {activeTab === 'tabPlot' && <div className="text-ac-white">{plot}</div>}
+              {activeTab === 'tabCast' && 
+                  <div className="text-ac-white d-flex flex-wrap">
+                      { actors.map((actor, index) => (
+                          <p key={index} className="movie-card-cast">{actor}</p>
+                      )) }
+                  </div>
+              }
+              {activeTab === 'tabGenre' && <div className="text-ac-white">{genre}</div>}
+          </div>
     </div>
   )
 }
