f2bb2affa17566030b2ee70a986963606a8f4458
Yuki Sano <clayuki@amazon.com>
2024-11-16 20:38:26 -0300

    Update CinemaType

--- front-end/src/types/cinema.tsx
+++ front-end/src/types/cinema.tsx
@@ -3,17 +3,19 @@ export interface CinemasType {
   cinemas: CinemaType[];
 };
 
-export interface CinemaType {
+export type CinemaType = {
   name: string;
-  location: string;
-  price: string;
-  address: {
-    home: { street: string; city: string };
-    destination: { street: string; city: string };
-  };
-  schedule: { time: string; subs: string }[];
-  commuteInfo: {
-    bestRoute: { time: string; transportation: string };
-    shortestDistance: string;
-  };
+  latitude: string;
+  longitude: string;
+  location?: string; // mocked for now
+  price?: string; // mocked for now
+  address?: {
+      home: { street: string; city: string };
+      destination: { street: string; city: string };
+  }; // mocked for now
+  schedule?: { time: string; subs: string }[]; // mocked for now
+  commuteInfo?: {
+      bestRoute: { time: string; transportation: string };
+      shortestDistance: string;
+  }; // mocked for now
 };
