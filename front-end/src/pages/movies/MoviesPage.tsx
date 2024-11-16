281bc5b7afdcdd7eb57708842ed301bd8deedc11
juliamelots <juliamelots@ibm.com>
2024-11-16 17:53:50 -0300

    Request back-end for available movies

--- front-end/src/pages/movies/MoviesPage.tsx
+++ front-end/src/pages/movies/MoviesPage.tsx
@@ -1,4 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+
+import axios from "@/services/axiosInstance";
+import { BackMovieType } from '@/types/types';
 
 import MoviesFilterModal from '@/components/ui/modal/MoviesFilterModal';
 import Navbar from '@/components/layout/navbar/Navbar';
@@ -7,31 +11,25 @@ import SearchBar from '@/components/layout/search-bar/SearchBar';
 import './MoviesPage.css';
 
 function MoviesPage() {
-  {/* TODO: Remove constant and call back-end */}
-  const movies = [
-    {
-      title: 'As Pontes de Madison',
-      posterUrl: 'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/90/90/74/20119562.jpg',
-      genres: ['Drama'],
-      rating: '14',
-    },
-    {
-      title: 'Forrest Gump - O Contador de Histórias',
-      posterUrl: 'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/87/30/21/19874092.jpg',
-      genres: ['Drama', 'Comédia'],
-      rating: '14',
-    },
-    {
-      title: 'Top Gun: Maverick',
-      posterUrl: 'https://br.web.img3.acsta.net/c_310_420/pictures/19/12/16/15/00/5548914.jpg',
-      genres: ['Ação'],
-      rating: '12',
-    },
-  ];
+  const [movies, setMovies] = useState<BackMovieType[]>([]);
+  const [error, setError] = useState<string>('');
 
   const [searchInput, setSearchInput] = useState<string>('');
   const [ratingFilters, setRatingFilters] = useState<string[]>([]);
   const [genreFilters, setGenreFilters] = useState<string[]>([]);
+
+    useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(`/movie`);
+        setMovies(response.data.movies as BackMovieType[]);
+      } catch (err) {
+        setError('Não foi possível carregar filmes.');
+        console.error(err);
+      }
+    };
+    fetchMovies();
+  }, []);
 
   const onFiltersSaved = (genres: Set<string>, ratings: Set<string>) => {
     setGenreFilters(Array.from(genres));
@@ -40,20 +38,33 @@ function MoviesPage() {
 
   const moviesGrid = movies
     .filter((movie) => {
-      const lowerMovieTitle = movie.title.toLocaleLowerCase();
+      const lowerMovieTitle = movie.name.toLocaleLowerCase();
       const lowerSearchInput = searchInput.toLocaleLowerCase();
       const searchResult = searchInput === '' || lowerMovieTitle.includes(lowerSearchInput);
 
-      const genreFiltersResult = (genreFilters.length === 0) || movie.genres.some((genre: string) => genreFilters.includes(genre));
+      const genreFiltersResult = (genreFilters.length === 0) || movie.genre.some((genre: string) => genreFilters.includes(genre));
       const ratingFiltersResult = (ratingFilters.length === 0) || ratingFilters.includes(movie.rating);
 
-      return searchResult && genreFiltersResult && ratingFiltersResult;
+      const result = searchResult && genreFiltersResult && ratingFiltersResult;
+      if (!result) {
+        setError('Não foi possível encontrar filmes que se encaixam em sua pesquisa.');
+      }
+      return result;
     })
     .map((movie, idx) =>
-      <div key={idx} className="movie-poster-size position-relative d-flex justify-content-center">
-        <img src={movie.posterUrl} alt={'Cartaz de "' + movie.title + '"'} className="movie-poster-size movie-poster" />
-        <p className="movie-title text-center">{movie.title}</p>
-      </div>
+      <Link
+        key={idx}
+        className="movie-poster-size position-relative d-flex justify-content-center"
+        to={`/movie/${movie.name}`}
+        state={{ movie: movie }}
+      >
+        <img
+          className="movie-poster-size movie-poster"
+          src={movie.img_url}
+          alt={'Cartaz de "' + movie.name + '"'}
+        />
+        <p className="movie-title text-center">{movie.name}</p>
+      </Link>
     );
 
   return (
@@ -64,7 +75,11 @@ function MoviesPage() {
           <h1 className="text-center text-white mt-2 mb-5">Filmes</h1>
           <SearchBar
             placeholder="Procure filmes por título"
-            filterModal={<MoviesFilterModal initialCheckedGenres={genreFilters} initialCheckedRatings={ratingFilters} onSave={onFiltersSaved}/>}
+            filterModal={<MoviesFilterModal
+              initialCheckedGenres={genreFilters}
+              initialCheckedRatings={ratingFilters}
+              onSave={onFiltersSaved}
+            />}
             searchInput={searchInput}
             setSearchInput={setSearchInput}
           />
@@ -76,7 +91,7 @@ function MoviesPage() {
               {moviesGrid}
             </div>
           ) : (
-            <p className="text-white text-center fs-5 mt-5">Não foi possível encontrar filmes que se encaixam em sua pesquisa.</p>
+            <p className="text-white text-center fs-5 mt-5">{error}</p>
           )}
         </div>
       </div>
