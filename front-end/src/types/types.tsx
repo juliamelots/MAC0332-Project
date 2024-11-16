9b012dfbfe3193b66112777d4ade9c2d6638844c
juliamelots <juliamelots@ibm.com>
2024-11-16 17:52:19 -0300

    Add movie type compatible to back-end response structure

--- front-end/src/types/types.tsx
+++ front-end/src/types/types.tsx
@@ -1,16 +1,29 @@
-export type CinemaType = {
+export interface CinemaType {
     name: string;
     latitude: string;
     longitude: string;
 };
   
-export type MovieType = {
-    title: string,
-    posterUrl: string,
-    runtime: string,
-    genre: string,
-    plot: string,
-    rated: string,
-    directors: string,
+export interface MovieType {
+    title: string;
+    posterUrl: string;
+    runtime: string;
+    genre: string;
+    plot: string;
+    rated: MovieRating;
+    directors: string;
     availableCinemas: CinemaType[];
 };
+
+export interface BackMovieType {
+    name: string;
+    availableCinemas: CinemaType[];
+    genre: string[];
+    rating: MovieRating;
+    duration: string;
+    synopsis: string;
+    url: string;
+    img_url: string;
+};
+
+type MovieRating = 'L' | '10' | '12' | '14' | '16' | '18';
