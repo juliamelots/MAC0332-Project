5064642273da2035fc78167fc34dfb1676b56ede
Francisco Henriques <franciscolealhenriques@usp.br>
2024-11-16 21:28:04 -0300

    Add routes to get available cinemas for a movie and list of all genres
    Now, the back-end can provide information of which cinemas a given movie
    is available, and all the genres possible for filtering the search.

--- backend/src/main/kotlin/com/rotadacultura/routes/MovieRoutes.kt
+++ backend/src/main/kotlin/com/rotadacultura/routes/MovieRoutes.kt
@@ -1,6 +1,7 @@
 package com.rotadacultura.routes
 
 import com.rotadacultura.model.Movie
+import com.rotadacultura.model.Session
 import io.ktor.server.response.*
 import io.ktor.http.*
 import io.ktor.server.routing.*
@@ -14,6 +15,31 @@ fun Route.movieRouting() {
                 call.respond(mapOf("movies" to movies))
             } catch (ex: Exception) {
                 call.respond(HttpStatusCode.InternalServerError, "Error gathering movies on display information: ${ex.message}")
+            }
+        }
+        get("{movie_id}/available-cinemas") {
+            try {
+                val movieId = call.parameters["movie_id"]!!.toString()
+                val sessions = Session.getSessionsData()
+                val cinemas = sessions
+                    .filter {it.movie == movieId}
+                    .map {it.cinema}
+                    .distinct()
+                call.respond(mapOf("available_cinemas" to cinemas))
+            } catch (ex: Exception) {
+                call.respond(HttpStatusCode.InternalServerError, "Error gathering available cinemas: ${ex.message}")
+            }
+        }
+        get("genres") {
+            try {
+                val movies = Movie.getMoviesData()
+                val genres = movies
+                    .flatMap { it.categories }
+                    .filter { it.isNotEmpty() }
+                    .distinct()
+                call.respond(mapOf("genres" to genres))
+            } catch (ex: Exception) {
+                call.respond(HttpStatusCode.InternalServerError, "Error gathering movie genres: ${ex.message}")
             }
         }
     }
