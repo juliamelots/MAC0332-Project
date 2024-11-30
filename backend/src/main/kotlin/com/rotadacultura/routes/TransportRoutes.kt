c5e7fd53a2a2643f73c437fc5e2a9724fc51bd80
Francisco Henriques <franciscolealhenriques@usp.br>
2024-11-30 15:46:15 -0300

    Update TransportRoutes to return optimal route on GET request

--- backend/src/main/kotlin/com/rotadacultura/routes/TransportRoutes.kt
+++ backend/src/main/kotlin/com/rotadacultura/routes/TransportRoutes.kt
@@ -1,5 +1,8 @@
 package com.rotadacultura.routes
 
+import com.rotadacultura.model.Cinema
+import com.rotadacultura.services.RouteCalculator
+import com.rotadacultura.services.RouteCalculatorSingleton
 import io.ktor.server.response.*
 import io.ktor.http.*
 import io.ktor.server.routing.*
@@ -9,12 +12,22 @@ fun Route.transportRouting() {
     route("/route") {
         get {
             try {
-                val userLatitude = call.request.queryParameters["user-latitude"]
-                val userLongitude = call.request.queryParameters["user-longitude"]
-                if (userLatitude == null || userLongitude == null) {
-                    call.respond(HttpStatusCode.BadRequest, "Missing required parameters: user latitude or longitude")
+                val userLatitude = call.request.queryParameters["user-latitude"]?.toDoubleOrNull()
+                val userLongitude = call.request.queryParameters["user-longitude"]?.toDoubleOrNull()
+                val cinemaName = call.request.queryParameters["cinema"]
+                if (userLatitude == null || userLongitude == null || cinemaName == null) {
+                    call.respond(HttpStatusCode.BadRequest, "Missing required parameters")
                     return@get
                 }
+                val cinema = Cinema.new(cinemaName) ?: throw IllegalStateException("Cinema not found")
+                val routeCalculator = RouteCalculatorSingleton.instance
+                val route = routeCalculator.calculateRoute(
+                    userLatitude,
+                    userLongitude,
+                    cinema.coordinates[0],
+                    cinema.coordinates[0]
+                )
+                call.respond(HttpStatusCode.OK, route)
             } catch (e: Exception) {
                 call.respond(HttpStatusCode.InternalServerError, "Could not calculate transport route: ${e.message}")
             }
