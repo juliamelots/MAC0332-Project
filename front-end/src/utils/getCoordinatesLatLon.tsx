2fae7fdaccb3db0985cca8b4e28a9c07e534ff24
natalyaaragao <natalya.saragao@gmail.com>
2024-12-02 19:31:07 -0300

    Fix getCoordinatesLatLon

--- front-end/src/utils/getCoordinatesLatLon.tsx
+++ front-end/src/utils/getCoordinatesLatLon.tsx
@@ -1,15 +1,28 @@
 import axios from "axios";
 
+interface OpenStreetMapResponse {
+    lat: string;
+    lon: string;
+}
+
 export const getCoordinatesLatLon = async (address: string) => {
-    const response = await axios.get("https://nominatim.openstreetmap.org/search", {
-        params: { q: address, format: "json" },
-    });
-  
-    if (response.data && response.data.length > 0) {
-        const { lat, lon } = response.data[0];
-        return { lat: parseFloat(lat), lon: parseFloat(lon) };
-    } else {
-        console.error("Address not found");
+    try {
+        const response = await axios.get<OpenStreetMapResponse[]>(
+            "https://nominatim.openstreetmap.org/search",
+            {
+                params: { q: address, format: "json" },
+            }
+        );
+    
+        if (response.data && response.data.length > 0) {
+            const { lat, lon } = response.data[0];
+            return { lat: parseFloat(lat), lon: parseFloat(lon) };
+        } else {
+            console.error("Address not found");
+            return { lat: 0, lon: 0 };
+        }
+    } catch (error) {
+        console.error("Error fetching coordinates:", error);
         return { lat: 0, lon: 0 };
     }
 };
