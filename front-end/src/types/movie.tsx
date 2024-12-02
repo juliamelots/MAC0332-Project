7b009845964d4ffc32df9c735ffe3835cad573e8
natalyaaragao <natalya.saragao@gmail.com>
2024-12-02 13:30:28 -0300

    Style movie rating

--- front-end/src/types/movie.tsx
+++ front-end/src/types/movie.tsx
@@ -9,4 +9,4 @@ export interface MovieType {
     img_url: string;
 };
 
-type MovieRating = 'L' | '10' | '12' | '14' | '16' | '18';
+export type MovieRating = 'L' | '10' | '12' | '14' | '16' | '18';
